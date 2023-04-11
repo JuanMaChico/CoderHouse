659190cbc93f858a40241d3e1d2e4c80cbc0352d
JuanMaChico <juanmanuelchico@hotmail.com>
2023-04-11 19:25:43 -0300

    Add proyecto general del curso

---
+++ CursoReact[CoderHouse]/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ItemListContainer from '../ItemListContainer/ItemListContainer';
+import './Background.css';
+
+const Home = () => {
+	return (
+		<div className="body">
+			<ItemListContainer />
+		</div>
+	);
+};
+
+export default Home;
---
+++ CursoReact[CoderHouse]/src/components/ItemDetail/ItemDetail.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ItemDetailContainer from '../ItemDetailContainer/ItemDetailContainer';
+
+const ItemDetail = () => {
+	return (
+		<div>
+			<ItemDetailContainer />
+		</div>
+	);
+};
+
+export default ItemDetail;
